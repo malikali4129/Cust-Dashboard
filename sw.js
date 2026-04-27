1b70fd28533e6834db01e4aff7f06701acf8d2d8
Ali Imran Malik <malikali4129@gmail.com>
2026-04-28 00:43:36 +0500

    update force

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v11';
+const CACHE_VERSION = 'v15';
 const SHELL_CACHE = `dashboard-shell-${CACHE_VERSION}`;
 const FONT_CACHE = `dashboard-fonts-${CACHE_VERSION}`;
 const DATA_CACHE = `dashboard-data-${CACHE_VERSION}`;
